ffbed6a6c565b97043bcd3c8677f2247949b1669
luciano <lucianojrluii@gmail.com>
2023-05-04 12:42:27 -0300

    commit dos exercicios

---
+++ exercicio1/script.js
@@ -0,0 +1,8 @@
+let nome = prompt ("qual é o seu nome?")
+let idade = Number(prompt ("qual é a sua idade"))
+
+console.log(typeof nome)
+
+console.log(typeof idade) 
+
+console.log("Ola,", nome, "voce  tem", idade, "anos")
---
+++ exercicio2/script.js
@@ -0,0 +1,11 @@
+let pergunta = 'voce e maior de idade?';
+let resposta = prompt(pergunta);
+
+let pergunta2 = 'voce e flamenguista?';
+let resposta2 = prompt(pergunta2);
+
+let pergunta3 = 'voce gosta de chocolate';
+let resposta3 = prompt(pergunta3); 
+
+console.log(pergunta,resposta,pergunta2,resposta2,pergunta3,resposta);
+
